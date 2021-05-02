318421bd559d68f7c08591485057d878b3318b22
SanthoshUppari <uppari.santhosh.kumar@gmail.com>
2021-05-02 19:46:49 +0530

    Updated

--- 005-springboot-oauth2/ehealth-api-gateway/src/main/java/com/amran/api/gateway/service/PersonAddressServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.amran.api.gateway.service;
-
-/**
- * @Author : Amran Hosssain on 6/24/2020
- */
-public class PersonAddressServiceImpl implements PersonAddressService{
-}
---
+++ 006-springboot-basic-authentication/src/main/java/com/usk/demo/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.usk.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@GetMapping("/test")
+	public ResponseEntity<String> test() {
+		return new ResponseEntity<>("success", HttpStatus.OK);
+	}
+}
